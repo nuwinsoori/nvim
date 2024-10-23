7f8ea7c72fcfb6bd990397c27897ed28ef9c4535
nuwin <nuwinsoori@gmail.com>
2024-10-24 00:34:47 +1030

    keybind changes

--- lua/nuwin/core/keymaps.lua
+++ lua/nuwin/core/keymaps.lua
@@ -10,14 +10,12 @@ keymap.set("v", "<C-b>", 'c**<C-r>"**<Esc>', { desc = "make bold" })
 keymap.set("i", "<C-b>", "****<Left><Left>", { desc = "bold insert mode" })
 keymap.set("i", "<C-e>", "**<Left>", { desc = "italics insert" })
 keymap.set("v", "<C-e>", 'c*<C-r>"*<Esc>', { desc = "italicise" })
-
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights" })
-keymap.set("n", "J", "<nop>", { desc = "Unbind J in normal mode" }) -- removes joining lines in normal mode
-keymap.set("v", "J", "<nop>", { desc = "Unbind J in visual mode" }) -- removes joining lines in normal mode
+keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>=", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
@@ -29,6 +27,14 @@ keymap.set("n", "<leader>tw", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Other
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights" })
+keymap.set("n", "J", "<nop>", { desc = "Unbind J in normal mode" })
+keymap.set("v", "J", "<nop>", { desc = "Unbind J in visual mode" })
+keymap.set("n", "ci}", "f{ci{", { desc = "Change in inner" })
+keymap.set("n", "di}", "f{di{", { desc = "Delete in inner" })
+keymap.set("n", "si}", "f{si{", { desc = "Substitute in inner" })
 
 -- MacOS
 keymap.set("i", "<A-BS>", "<C-w>", { desc = "Delete last word in insert mode with Option+Delete" })
